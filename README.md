922680c3621c95342295001465a57531dced8c2b
erindc <erindcox6@gmail.com>
2019-01-20 14:49:11 -0600

    uploading completed assignment

---
+++ README.md
@@ -0,0 +1,21 @@
+#simple base64 encoding and decoding
+------
+
+A simple tool to encode, and optionally decode, a message.
+
+Syntax highlighting for ease of use has been added.
+
+------
+
+###to pull down and run the code...
+------
+
+`git clone https://github.com/erindc/week-1-package-managed-project.git`
+
+`cd week-1-package-managed-project`
+
+`pipenv install`
+
+`pipenv run python3 ./main.py`
+
+
---
+++ main.py
@@ -0,0 +1,26 @@
+import pybase64
+from colorama import init
+init()
+from colorama import Fore, Back
+
+def decode(base64):
+    decode = ""
+    while decode!= "y" or decode != "n":
+        print(Fore.RESET)
+        decode = input("Do you want to decode? (y/n): ")
+        if decode == "y":
+            print(Fore.CYAN, pybase64.b64decode(base64).decode("utf-8"))
+            exit()
+        elif decode == "n":
+            exit()
+        else:
+            print(Fore.RED, "Invalid option! Please enter y or n")
+
+def main():
+    text = input("Enter something to encode: ").encode()
+    base64 = pybase64.b64encode(text)
+    string = base64.decode("utf-8")
+    print(Fore.CYAN, 'base64 encoding: ', string)
+    decode(base64)
+
+main()
