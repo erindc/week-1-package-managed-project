d7b2eb2e5acab1ff5ebeab90d1a780e228c549a5
erindc <erindcox6@gmail.com>
2019-01-20 14:53:41 -0600

    update variable name

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def decode(base64):
             print(Fore.RED, "Invalid option! Please enter y or n")
 
 def main():
-    text = input("Enter something to encode: ").encode()
-    base64 = pybase64.b64encode(text)
+    byteInput = input("Enter something to encode: ").encode()
+    base64 = pybase64.b64encode(byteInput)
     string = base64.decode("utf-8")
     print(Fore.CYAN, 'base64 encoding: ', string)
     decode(base64)
